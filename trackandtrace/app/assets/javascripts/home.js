1d6615eef9d69160f1e7885a303715a4980048bd
Lucas SIRIGNANO <lsirigna@e2r2p20.42.fr>
2016-03-05 00:15:54 +0100

    autocommit

--- trackandtrace/app/assets/javascripts/home.js
+++ trackandtrace/app/assets/javascripts/home.js
@@ -38,8 +38,11 @@ angular.module('mapsApp', [])
                 //https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_poincont2&geofilter.distance=48.8520930694,2.34738897685,500
                 $http.get("https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_poincont2&geofilter.distance=" + longitude + "," + latitude + ",1000").then(function(rsp)
                 {
+                    arrcities[n] = [];
+                        console.log('s');
                     // rsp.data.records
                     for (record in rsp.data.records) {
+                        console.log('l');
                         arrcities[n].push({city: record.fields.libelle_du_site, desc: record.fields.adresse + " "  + record.fields.code_postal + ", "  + record.fields.localite, lat: record.fields.latitude, long: record.fields.longitude});
                     }
                 }, function() {});
