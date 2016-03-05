fec5e0590469e751df29b6cec14c16fcb4079255
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-05 23:20:11 +0100

    autocommit

--- old/trackandtrace/db/migrate/20160303143541_create_valises.rb
+++ old/trackandtrace/db/migrate/20160303143541_create_valises.rb
@@ -5,15 +5,19 @@ class CreateValises < ActiveRecord::Migration
       t.references :trajet_id
       t.integer :mouvement
       t.integer :prix
-      t.text :last_address
-      t.text :final_address
-      t.text :start_address
+      t.integer :nbr_petit
+      t.integer :nbr_moyen
+      t.integer :nbr_gros
+      t.integer :state
       t.text :final_address_coord
       t.text :start_address_coord
-      t.text :start_relai_addr
+      t.text :start_addr
+      t.text :final_addr
+      t.text :first_relai_addr
       t.text :final_relai_addr
-      t.text :start_relai_coord
+      t.text :first_relai_coord
       t.text :final_relai_coord
+      t.integer :numero_de_suivi
       t.datetime :last_ping
       t.datetime :depot_date
       t.datetime :retrait_date
