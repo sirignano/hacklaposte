bdcf217c3a8e7fc550bc0171fa0bb2577b747ef3
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-05 22:03:11 +0100

    autocommit

--- old/trackandtrace/app/assets/javascripts/collections/order-collection.js
+++ old/trackandtrace/app/assets/javascripts/collections/order-collection.js
@@ -1,10 +1,1 @@
 'use strict'
-
-var Tweet = require('../models/tweet-model');
-
-var TweetsCollection = Backbone.Collection.extend({
-		model: Tweet,
-		url: '/api/posts'
-	});
-
-module.exports = TweetsCollection;
