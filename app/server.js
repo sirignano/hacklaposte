35eea29fdd4718459061f90886d091d26c2307f3
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-05 18:44:10 +0100

    autocommit

--- app/server.js
+++ app/server.js
@@ -1,69 +1,110 @@
-// Node Require
-var http = require("http");
-var fs = require("fs");
+// // Node Require
+// var http = require("http");
+// var fs = require("fs");
 
-// External Require
-var mime = require("mime");
-var express = require("express");
-var randomstring = require("randomstring");
+// // External Require
+// var mime = require("mime");
+// var express = require("express");
+// var randomstring = require("randomstring");
 
-// Database Require
-var MongoClient = require("mongodb").MongoClient;
-var MongoObjectID = require("mongodb").ObjectID;
+// // Database Require
+// var MongoClient = require("mongodb").MongoClient;
+// var MongoObjectID = require("mongodb").ObjectID;
 
 
-// Middlewares Require
-var logger = require("morgan");
-var serveStatic = require("serve-static");
-var favicon = require("serve-favicon");
-var bodyParser = require("body-parser");
+// // Middlewares Require
+// var logger = require("morgan");
+// var serveStatic = require("serve-static");
+// var favicon = require("serve-favicon");
+// var bodyParser = require("body-parser");
 
-// Global variable
-var port = 8080;
+// // Global variable
+// var port = 8080;
 
-// Middleware
+// // Middleware
+// var passport = require('passport');
+// var app = express();
+
+// app.use(logger(":method :url"));
+// app.use(serveStatic(__dirname + "/app"));
+
+// // API
+// var api = express();
+
+// api.get("/get_trajet", function (req, res) {
+// 	res.header("Access-Control-Allow-Origin", "*");	
+// 	res.send({trajet: ""});
+// });
+
+// app.use(bodyParser({limit: '5mb'}))
+
+// api.post("/add_trajet", function (req, res) {
+// 	res.send({trajet: ""});
+// });
+
+// app.use("/api", api);
+
+// http.createServer(app).listen(port);
+
+// console.log("Serveur démarré sur le port " + port);
+
+// passport.use(new BasicStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return done(err); }
+//       if (!user) { return done(null, false); }
+//       if (!user.validPassword(password)) { return done(null, false); }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+// app.get('/api/me',
+//   passport.authenticate('basic', { session: false }),
+//   function(req, res) {
+//     res.json(req.user);
+//   });
+
+// console.log('Hello');
+// console.log(passport);
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
 var passport = require('passport');
-var app = express();
+var flash    = require('connect-flash');
 
-app.use(logger(":method :url"));
-app.use(serveStatic(__dirname + "/app"));
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-// API
-var api = express();
+var configDB = require('./config/database.js');
 
-api.get("/get_trajet", function (req, res) {
-	res.header("Access-Control-Allow-Origin", "*");	
-	res.send({trajet: ""});
-});
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-app.use(bodyParser({limit: '5mb'}))
+// require('./config/passport')(passport); // pass passport for configuration
 
-api.post("/add_trajet", function (req, res) {
-	res.send({trajet: ""});
-});
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-app.use("/api", api);
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-http.createServer(app).listen(port);
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-console.log("Serveur démarré sur le port " + port);
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.validPassword(password)) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-
-app.get('/api/me',
-  passport.authenticate('basic', { session: false }),
-  function(req, res) {
-    res.json(req.user);
-  });
-
-console.log('Hello');
-console.log(passport);
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
