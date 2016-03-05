36b17d39ad74ff2737d8bf957c0e6c1b43d383a2
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-05 22:25:12 +0100

    autocommit

--- old/trackandtrace/config/application.rb
+++ old/trackandtrace/config/application.rb
@@ -22,6 +22,5 @@ module Trackandtrace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.browserify_rails.commandline_options = "-t [ reactify --es6 --extension hbs ]"
   end
 end
