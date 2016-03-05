fc67608e59f31280af19213096cb15d427695585
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-05 18:38:27 +0100

    autocommit

--- app/server.js
+++ app/server.js
@@ -22,6 +22,7 @@ var bodyParser = require("body-parser");
 var port = 8080;
 
 // Middleware
+var passport = require('passport');
 var app = express();
 
 app.use(logger(":method :url"));
@@ -46,3 +47,23 @@ app.use("/api", api);
 http.createServer(app).listen(port);
 
 console.log("Serveur démarré sur le port " + port);
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.validPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+app.get('/api/me',
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+    res.json(req.user);
+  });
+
+console.log('Hello');
+console.log(passport);
