e091c535adaa5b258e1530ab71c43719c63db707
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-05 19:37:27 +0100

    autocommit

--- app/config/database.js
+++ app/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongo://root:hacklaposte@198.211.105.132:27017/objects'
 
 };
