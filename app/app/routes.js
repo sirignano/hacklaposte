f6b0c6d6c5921737916deb0adc0ec55adae0d4f8
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-05 20:01:36 +0100

    autocommit

--- app/app/routes.js
+++ app/app/routes.js
@@ -51,13 +51,21 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
-    
+
      // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+    
 };
 
 // route middleware to make sure a user is logged in
