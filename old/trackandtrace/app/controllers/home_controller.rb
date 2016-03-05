9951bebdaeffeedbe51c403303066077e8460aa2
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-06 00:55:48 +0100

    autocommit

--- old/trackandtrace/app/controllers/home_controller.rb
+++ old/trackandtrace/app/controllers/home_controller.rb
@@ -16,24 +16,24 @@ class HomeController < ApplicationController
   def suivi
   		begin
 			a = Valise.where("numero_de_suivi == #{params[:id]}")
-		if a.empty?
-			render :text => "{err1}" and return
-		else
-			str = "{"
-			b = a.first
-			if b.state == 0
-				str += '{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
-			elsif b.state == 1
-				str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
-			elsif b.state == 2
-				str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
-			elsif b.state == 3
-				str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"}}'
-			elsif b.state == 4
-				str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"}}'
+			if a.empty?
+				render :text => "{err1}" and return
+			else
+				str = "{"
+				b = a.first
+				if b.state == 0
+					str += '{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
+				elsif b.state == 1
+					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
+				elsif b.state == 2
+					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}}'
+				elsif b.state == 3
+					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"}}'
+				elsif b.state == 4
+					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"}}'
+				end
+				render :text => str and return
 			end
-			render :text => str and return
-		end
 		rescue
 			render :text => "{err3}" and return
 		end
