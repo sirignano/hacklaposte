53f18539ee87901084e7826f8027353f7ad5d163
elytum <arthur.chazal@gmail.com>
2016-03-06 05:04:53 +0100

    ...

--- old/trackandtrace/app/assets/javascripts/home.js
+++ old/trackandtrace/app/assets/javascripts/home.js
@@ -22,7 +22,7 @@ var createMarker = function (info){
 
   console.log(infowindow);
   google.maps.event.addListener(marker, 'click', function(){
-      infoWindow.setContent('<h2>' + marker.city + '</h2>' + marker.desc);
+      infowindow.setContent('<h2>' + marker.city + '</h2>' + marker.desc);
       infowindow.open(map, marker);
   });
 
@@ -47,8 +47,8 @@ function initMap(step) {
     center: {lat: latitude, lng: longitude},
     zoom: 12
   });
-  infowindow = new google.maps.InfoWindow({
-  });
+  // infowindow = new google.maps.InfoWindow({
+  // });
   console.log(infowindow);
   xhr.open("GET", 'https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_poincont2&geofilter.distance=' + latitude + ',' + longitude + ',5000', false);
   xhr.send();
