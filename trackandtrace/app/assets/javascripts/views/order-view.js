b99101187254aebc9bf0285973c80e4283651f8c
elytum <arthur.chazal@gmail.com>
2016-03-06 08:31:06 +0100

    ...

--- trackandtrace/app/assets/javascripts/views/order-view.js
+++ trackandtrace/app/assets/javascripts/views/order-view.js
@@ -54,7 +54,7 @@ var OrderView = Marionette.ItemView.extend({
 		},
 
 		getResult: function(e) {
-			console.log(document.getElementById('fromRecapAddr');
+			console.log(document.getElementById('fromRecapAddr'));
 		   this.trigger('getResult', e);
 		},
 
