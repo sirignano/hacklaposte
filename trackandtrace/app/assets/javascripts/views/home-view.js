27ed3ff3d8f63f96b2fe0cefb0485418ceebb2ca
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-06 13:38:08 +0100

    autocommit

--- trackandtrace/app/assets/javascripts/views/home-view.js
+++ trackandtrace/app/assets/javascripts/views/home-view.js
@@ -6,7 +6,11 @@ var tpl = require('../../templates/home.hbs');
 
 var HomeView = Marionette.ItemView.extend({
 		//el: '#tweet-container',
-		template: tpl
+		template: tpl,
+		initialize:	function () { 
+			$('#map').hide();
+		}
+
 	});
 
 module.exports = HomeView;
