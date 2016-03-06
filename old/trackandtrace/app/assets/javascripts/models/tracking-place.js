3c9f18d4a1640680b77cb637b544861480221436
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-06 03:51:57 +0100

    autocommit

--- old/trackandtrace/app/assets/javascripts/models/tracking-place.js
+++ old/trackandtrace/app/assets/javascripts/models/tracking-place.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var TrackingPlace = Backbone.Model.extend({
-		urlRoot: 'api.laposte.fr/suivi/v1',
+		urlRoot: 'http://api.laposte.fr/suivi/v1',
 		defaults: function() {
 			return  {
 				
