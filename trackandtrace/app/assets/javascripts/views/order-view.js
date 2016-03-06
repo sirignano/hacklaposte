55336c33eb043ae563eec94575ea8596dab1a3ee
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-06 04:29:58 +0100

    autocommit

---
+++ trackandtrace/app/assets/javascripts/views/order-view.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//TEMPLATE
+var tpl = require('../../templates/order.hbs');
+
+var OrderView = Marionette.ItemView.extend({
+		//el: '#tweet-container',
+		template: tpl, 
+
+		ui: {
+			btnSubmit: "#btnSubmit"
+		},
+
+		events: {
+			'click @ui.btnSubmit': 'getResult'
+		},
+
+		getResult: function(e) {
+		   this.trigger('getResult', e);
+		}
+	});
+
+module.exports = OrderView;
