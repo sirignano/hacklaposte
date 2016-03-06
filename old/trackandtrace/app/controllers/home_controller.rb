0c611847b85066baabb78556a75ea5e71d7ba3fe
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-06 01:26:50 +0100

    autocommit

--- old/trackandtrace/app/controllers/home_controller.rb
+++ old/trackandtrace/app/controllers/home_controller.rb
@@ -21,17 +21,28 @@ class HomeController < ApplicationController
 				str = "["
 				b = a.first
 				if b.state == 0
-					str += '{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}]'
+					str += '{"status": "true", "desc": "adresse_de_depart"},{"status": "false", "desc": "ville"},{"status": "false", "desc": ""},{"status": "false", "desc": "ville"},{"status": "false", "desc": "adresse_de_arrive"}]'
 				elsif b.state == 1
-					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}]'
+					str += '{"status": "true", "desc": "adresse_de_depart"},{"status": "true", "desc": "ville"},{"status": "false", "desc": ""},{"status": "false", "desc": "ville"},{"status": "false", "desc": "adresse_de_arrive"}]'
 				elsif b.state == 2
-					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"},{"status": "false", "desc": "none"}]'
+					str += '{"status": "true", "desc": "adresse_de_depart"},{"status": "true", "desc": "ville"},{"status": "true", "desc": ""},{"status": "false", "desc": "ville"},{"status": "false", "desc": "adresse_de_arrive"}]'
 				elsif b.state == 3
-					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "false", "desc": "none"}]'
+					str += '{"status": "true", "desc": "adresse_de_depart"},{"status": "true", "desc": "ville"},{"status": "true", "desc": ""},{"status": "true", "desc": "ville"},{"status": "false", "desc": "adresse_de_arrive"}]'
 				elsif b.state == 4
-					str += '{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"},{"status": "true", "desc": "none"}]'
+					str += '{"status": "true", "desc": "adresse_de_depart"},{"status": "true", "desc": "ville"},{"status": "true", "desc": ""},{"status": "true", "desc": "ville"},{"status": "true", "desc": "adresse_de_arrive"}]'
 				end
 				render :text => str and return
 			end
+			# 0
+			# adresse_de_depart 
+			# 1
+			# ville only
+			# 2
+			# rien
+			# 3
+			# ville
+			# 4
+			# adresse d'arrivée
+
   end
 end
